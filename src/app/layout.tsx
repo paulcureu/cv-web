3d8ef06e743ae91083054bd6d572434657c12478
Paul <cureu.paul.ioan@gmail.com>
2025-06-10 21:21:42 +0300

    feat: Implement custom favicon and update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,7 @@ export const metadata: Metadata = {
     title: "BalkanCode",
     description: "Portofoliul personal al lui Paul C., un dezvoltator pasionat.",
     icons: {
-        icon: '/balkancode-logo-ico.png', // <-- Asigură-te că numele fișierului și calea sunt corecte
-        // Poți adăuga și alte dimensiuni sau tipuri de iconițe dacă vrei
-        // apple: '/apple-icon.png',
+        icon: '/balkancode-logo-ico.png',
     },
 };
 
